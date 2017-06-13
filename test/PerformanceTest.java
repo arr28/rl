4a03c1bbe76211a32c0ceae85b39df7198f77d19
Andrew Rose <andrewrose+github@gmail.com>
2017-06-13 21:07:05 +0100

    Improved test framework

--- test/PerformanceTest.java
+++ test/PerformanceTest.java
@@ -24,8 +24,18 @@ import me.arr28.mcts.policy.tree.SimpleUpdatePolicy;
 import me.arr28.mcts.policy.tree.UCB1ApproxSelectPolicy;
 import me.arr28.mcts.policy.tree.UCB1SelectPolicy;
 
-public class TestHarness
+public class PerformanceTest
 {
+  /**
+   * Configuration.
+   */
+  private static final int MAX_THREADS = 4;
+  private static final int NUM_REPEATS = 3;
+  private static final int NUM_PURE_ROLLOUTS = 1_000_000;
+  private static final int NUM_MCTS_ITERATIONS = MCTSTree.NODE_POOL_SIZE - 1;
+  private static final Policies POLICY_SET = Policies.UCT_APPROX;
+  private static final Game GAME = Game.CONNECT4;
+
   private static enum Policies
   {
     SUPER_SIMPLE, UCT, UCT_APPROX, EXP3, TREE_SPEED;
@@ -36,20 +46,25 @@ public class TestHarness
     CONNECT4, HEX, BREAKTHROUGH;
   }
 
-  private static final int NUM_THREADS = 3;
-  private static final int NUM_PURE_ROLLOUTS = 0; //10_000_000;
-  private static final int NUM_MCTS_ITERATIONS = MCTSTree.NODE_POOL_SIZE - 1;
-  private static final Policies POLICY_SET = Policies.UCT_APPROX;
-  private static final Game GAME = Game.CONNECT4;
+  private final SelectPolicy lSelectPolicy;
+  private final ExpandPolicy lExpandPolicy;
+  private final RolloutPolicy lRolloutPolicy;
+  private final UpdatePolicy lUpdatePolicy;
+  private final GameStateFactory mGameStateFactory;
+  private final ScoreBoardFactory lScoreBoardFactory;
 
-  public static void main(String[] xiArgs) throws InterruptedException
+  public static void main(String[] xiArgs)
   {
-    SelectPolicy lSelectPolicy;
-    ExpandPolicy lExpandPolicy;
-    RolloutPolicy lRolloutPolicy;
-    UpdatePolicy lUpdatePolicy;
+    PerformanceTest lTest = new PerformanceTest();
+    lTest.testAll();
+  }
+
+  /**
+   * Configure the performance test.
+   */
+  public PerformanceTest()
+  {
     GameStateFactory lGameStateFactory = null;
-    ScoreBoardFactory lScoreBoardFactory;
 
     switch (POLICY_SET)
     {
@@ -109,25 +124,22 @@ public class TestHarness
       }
     }
 
-    MCTSTree lMCTS = new MCTSTree(lSelectPolicy,
-                                  lExpandPolicy,
-                                  lRolloutPolicy,
-                                  lUpdatePolicy,
-                                  lGameStateFactory,
-                                  lScoreBoardFactory);
+    mGameStateFactory = lGameStateFactory;
+  }
 
-    // Test full MCTS iteration speed.
-    Thread[] lWorker = new Thread[NUM_THREADS];
-    for (int lii = 0; lii < NUM_THREADS; lii++)
-    {
-      lWorker[lii] = new Thread(new MCTSWorker(lMCTS, NUM_MCTS_ITERATIONS / NUM_THREADS), "MCTS Worker " + lii);
-    }
+  private void testAll()
+  {
+    testPureRolloutSpeed();
+    testFullMCTSSpeed();
+  }
 
+  private void testPureRolloutSpeed()
+  {
     if (NUM_PURE_ROLLOUTS > 0)
     {
       System.out.println("Running " + NUM_PURE_ROLLOUTS + " pure rollouts (single-threaded)");
-      GameState lInitialState = lGameStateFactory.createInitialState();
-      GameState lRolloutState = lGameStateFactory.createInitialState();
+      GameState lInitialState = mGameStateFactory.createInitialState();
+      GameState lRolloutState = mGameStateFactory.createInitialState();
       long lStartTime = System.nanoTime();
       for (int lii = 0; lii < NUM_PURE_ROLLOUTS; lii++)
       {
@@ -139,23 +151,76 @@ public class TestHarness
       System.out.print(" = " + (lRolloutTime  / NUM_PURE_ROLLOUTS) + "ns per rollout");
       System.out.println(" = " + (long)(NUM_PURE_ROLLOUTS * ((double)1_000_000_000 / lRolloutTime)) + " rollouts/s");
     }
+  }
 
-    System.out.println("Starting MCTS iterations on " + NUM_THREADS + " threads");
-    long lStartTime = System.nanoTime();
-    for (int lii = 0; lii < NUM_THREADS; lii++)
+  private void testFullMCTSSpeed()
+  {
+    for (int lii = 1; lii <= MAX_THREADS; lii++)
     {
-      lWorker[lii].start();
+      System.out.println("=== Testing with " + lii + " thread(s) ===");
+      testFullMCTSSpeedNThreads(lii);
+      System.out.println();
     }
-    for (int lii = 0; lii < NUM_THREADS; lii++)
+  }
+
+  private void testFullMCTSSpeedNThreads(int xiNumThreads)
+  {
+    int lIterationsPerThread = NUM_MCTS_ITERATIONS / xiNumThreads;
+
+    // Test tree building
+    long lBestSpeed = 0;
+    for (int lTestRun = 0; lTestRun < NUM_REPEATS; lTestRun++)
     {
-      lWorker[lii].join();
+      System.gc();
+      MCTSTree lMCTS = new MCTSTree(lSelectPolicy,
+                                    lExpandPolicy,
+                                    lRolloutPolicy,
+                                    lUpdatePolicy,
+                                    mGameStateFactory,
+                                    lScoreBoardFactory);
+
+      // Test full MCTS iteration speed.
+      MCTSWorker[] lWorker = new MCTSWorker[xiNumThreads];
+      Thread[] lWorkerThread = new Thread[xiNumThreads];
+      for (int lii = 0; lii < xiNumThreads; lii++)
+      {
+        lWorker[lii] = new MCTSWorker(lMCTS, lIterationsPerThread);
+        lWorkerThread[lii] = new Thread(lWorker[lii], "MCTS Worker " + lii);
+      }
+
+      System.out.println("Starting MCTS iterations on " + xiNumThreads + " threads");
+      for (int lii = 0; lii < xiNumThreads; lii++)
+      {
+        lWorkerThread[lii].start();
+      }
+      long lTotalIterationTime = 0;
+      long lTotalIterationsPerSecond = 0;
+      for (int lii = 0; lii < xiNumThreads; lii++)
+      {
+        try
+        {
+          lWorkerThread[lii].join();
+        }
+        catch (InterruptedException ex)
+        {
+          throw new RuntimeException("Failed to join worker", ex);
+        }
+
+        long lIterationTime = lWorker[lii].getElapsedTime();
+        long lIterationsPerSecond = (long)(lIterationsPerThread * ((double)1_000_000_000 / lIterationTime));
+        lTotalIterationTime += lIterationTime;
+        lTotalIterationsPerSecond += lIterationsPerSecond;
+        System.out.println("  Worker " + lii + " performed " + lIterationsPerSecond + " iterations/s");
+      }
+      System.out.println("  Took " + (lTotalIterationTime / 1_000_000) + "ms total CPU time");
+      System.out.println("    = " + (lTotalIterationTime  / NUM_MCTS_ITERATIONS) + "ns per iteration");
+      System.out.println("    = " + lTotalIterationsPerSecond + " parallel iterations/s");
+      System.out.println("Average reward: " + lMCTS.getRoot().mScoreBoard.getAverageReward());
+      dumpRootStats(NUM_MCTS_ITERATIONS, lMCTS);
+      lBestSpeed = Math.max(lBestSpeed, lTotalIterationsPerSecond);
     }
-    long lIterationTime = System.nanoTime() - lStartTime;
-    System.out.print("  Took " + (lIterationTime / 1_000_000) + "ms");
-    System.out.print(" = " + (lIterationTime  / NUM_MCTS_ITERATIONS) + "ns per iteration");
-    System.out.println(" = " + (long)(NUM_MCTS_ITERATIONS * ((double)1_000_000_000 / lIterationTime)) + " iterations/s");
-    System.out.println("Average reward: " + lMCTS.getRoot().mScoreBoard.getAverageReward());
-    dumpRootStats(NUM_MCTS_ITERATIONS, lMCTS);
+
+    System.out.println("Best speed for " + xiNumThreads + " thread(s) was " + lBestSpeed + " iterations/s");
   }
 
   private static void dumpRootStats(int xiIteration, MCTSTree xiTree)
@@ -177,6 +242,7 @@ public class TestHarness
   {
     private final MCTSTree mTree;
     private final int mIterations;
+    private long mTime;
 
     public MCTSWorker(MCTSTree xiTree, int xiIterations)
     {
@@ -188,8 +254,14 @@ public class TestHarness
     public void run()
     {
       mTree.warm(mIterations);
+      long lStart = System.nanoTime();
       mTree.iterate(mIterations);
-      System.out.println("Worker complete @ " + System.currentTimeMillis());
+      mTime = System.nanoTime() - lStart;
+    }
+
+    public long getElapsedTime()
+    {
+      return mTime;
     }
   }
 }
