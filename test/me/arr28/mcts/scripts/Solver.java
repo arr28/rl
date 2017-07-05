eedd87bc33732d980199af98cf602fe8a2287111
Andrew Rose <andrewrose+github@gmail.com>
2017-07-05 23:10:55 +0100

    Detailed notes on bug in solver.
    Transposition table and alpha-beta pruning interact nastily.

--- test/me/arr28/mcts/scripts/Solver.java
+++ test/me/arr28/mcts/scripts/Solver.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 import me.arr28.game.GameState;
 import me.arr28.game.GameStateFactory;
-import me.arr28.games.Connect4State.C4GameStateFactory;
+import me.arr28.games.FastConnect4State.FastC4GameStateFactory;
 import me.arr28.mcts.zeroalloc.LRUCachable;
 import me.arr28.mcts.zeroalloc.LRUCache;
 
@@ -12,7 +12,7 @@ public class Solver
 {
   public static final int MAX_DEPTH = 50;
   public static final int MAX_ACTIONS = 7;
-  public static final int MAX_CACHE_SIZE = 5_000_000;
+  public static final int MAX_CACHE_SIZE = 10_000_000;
   public static final boolean DO_ONE_MOVE_LOOKAHEAD = true;
 
   public static class CachableState implements LRUCachable<GameState, CachableState>
@@ -35,11 +35,11 @@ public class Solver
     }
   }
 
-  private final GameStateFactory mGameStateFactory = new C4GameStateFactory();
+  private final GameStateFactory mGameStateFactory = new FastC4GameStateFactory();
   private final LRUCache<GameState, CachableState> mCache = new LRUCache<>(MAX_CACHE_SIZE);
 
   private long mTerminalStatesVisited;
-  private long mWinningPeeks;
+  private long mPeekResults;
   private int mSmallestCompleteDepth;
 
   private final GameState[][] mLookAhead = new GameState[MAX_DEPTH][MAX_ACTIONS];
@@ -62,13 +62,40 @@ public class Solver
       }
     }
 
-    double lValue = solve(mGameStateFactory.createInitialState(), 0, 0, 1);
-    log("Value of initial state is " + lValue);
+    GameState lInitialState = mGameStateFactory.createInitialState();
+    // --- Column numbering: 012345
+    // lInitialState.applyAction(5);
+
+    // !! When starting at the real root state, the 1-move scores are reported as
+    // !! loss, loss, draw, draw, draw, draw
+    // !!
+    // !! But when playing the first move manually, the 0-move scores are reported as
+    // !! loss, loss, loss, draw, draw, loss
+    // !!
+    // !! Which makes it look like there's some state corruption happening.
+    // !!
+    // !! Aha - turning of caching solves it.  I strongly suspect this is a nasty interaction
+    // !! between caching & alpha-beta pruning.  Due to a-b pruning, we might choose to stop looking when we find a draw
+    // !! even if the position is actually a win (because we know that the previous node can already hold us to a draw).
+    // !! That's fine in context.  But we would then cache the state as a draw (when it could still be a win).  Then we
+    // !! might use the cached state's value in a different context.
+    // !!
+    // !! 1 possible solution is to store the relevant a-b parameters and only use the cached value if the new context
+    // !! is the same as the context in which it was stored.  Otherwise do the full calculation and updated the cached
+    // !! value.  (Reusing cached results from a less constrained run is also okay.  And we only care about 1 of alpha or
+    // !! beta.)
+
+    double lValue = solve(lInitialState, 0, 0, 1);
+    log("Value of state is " + lValue + "\n" + lInitialState);
+    logStats();
   }
 
   private double solve(GameState xiState, int xiDepth, double xiAlpha, double xiBeta)
   {
-    boolean lDoCache = ((xiDepth % 4 == 0) && (xiDepth < 40));
+    // log("solve(depth=" + xiDepth + ", alpha=" + xiAlpha + ", beta=" + xiBeta + ")...\n" + xiState);
+
+    // boolean lDoCache = ((xiDepth % 4 == 0) && (xiDepth < 28));
+    boolean lDoCache = false; // !! ARR Enable caching
     if (lDoCache && mCache.contains(xiState))
     {
       return mCache.get(xiState).mReward;
@@ -76,10 +103,6 @@ public class Solver
 
     if (xiState.isTerminal())
     {
-      if (xiState.getReward() != 0.5)
-      {
-        throw new RuntimeException("Unexpected score " + xiState.getReward() + " in state:\n" + xiState);
-      }
       recordTerminal();
       return xiState.getReward();
     }
@@ -99,11 +122,11 @@ public class Solver
         GameState lPeekState = lLookAhead[lAction];
         xiState.copyTo(lPeekState);
         lPeekState.applyAction(lAction);
-        if ((lPeekState.isTerminal()) && (lPeekState.getReward() == 1))
+        if ((lPeekState.isTerminal()) && (lPeekState.getReward() >= xiBeta))
         {
-          mWinningPeeks++;
+          mPeekResults++;
           recordTerminal();
-          lBestReward = 1;
+          lBestReward = lPeekState.getReward();
           lFoundWin = true;
           break;
         }
@@ -132,11 +155,11 @@ public class Solver
         GameState lPeekState = lLookAhead[lAction];
         xiState.copyTo(lPeekState);
         lPeekState.applyAction(lAction);
-        if ((lPeekState.isTerminal()) && (lPeekState.getReward() == 0))
+        if ((lPeekState.isTerminal()) && (lPeekState.getReward() <= xiAlpha))
         {
-          mWinningPeeks++;
+          mPeekResults++;
           recordTerminal();
-          lBestReward = 0;
+          lBestReward = lPeekState.getReward();
           lFoundWin = true;
           break;
         }
@@ -155,10 +178,14 @@ public class Solver
       }
     }
 
-    if (xiDepth < mSmallestCompleteDepth)
+    if (xiDepth <= mSmallestCompleteDepth)
     {
       mSmallestCompleteDepth = xiDepth;
-      log("Completed a sub-tree at depth: " + mSmallestCompleteDepth);
+      log("Completed a sub-tree at depth: " + mSmallestCompleteDepth + " with result " + lBestReward /*+ "\n" + xiState*/);
+      if (xiDepth == 1)
+      {
+        log("\n" + xiState);
+      }
     }
 
     if (lDoCache)
@@ -179,6 +206,8 @@ public class Solver
       mCache.put(lCachableState.mState, lCachableState);
     }
 
+    // log("Solved (depth=" + xiDepth + ", alpha=" + xiAlpha + ", beta=" + xiBeta + ") with reward " + lBestReward + " for\n" + xiState);
+
     return lBestReward;
   }
 
@@ -186,12 +215,17 @@ public class Solver
   {
     if (++mTerminalStatesVisited % 100_000_000L == 0)
     {
-      log("Terminals, Wins, Cache Size, Hits, Evictions = " + mTerminalStatesVisited + ", " + mWinningPeeks + ", " + mCache.size() + ", " + mCache.mHits + ", " + mCache.mEvictions);
+      logStats();
     }
   }
 
   private static void log(String xiMessage)
   {
     System.out.println("[" + ZonedDateTime.now().format( DateTimeFormatter.ISO_INSTANT) + "] " + xiMessage);
+  }
+
+  private void logStats()
+  {
+    log("Terminals, Peeks, Cache Size, Hits, Evictions = " + mTerminalStatesVisited + ", " + mPeekResults + ", " + mCache.size() + ", " + mCache.mHits + ", " + mCache.mEvictions);
   }
 }
